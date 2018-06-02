39b7c353cd3bdf3a38e6840462fc777283b883d8
quanrobi <luciarobi29@gmail.com>
2018-06-02 10:00:41 +0300

    first commit

---
+++ try.py
@@ -0,0 +1,23 @@
+print ('hello world')
+
+my_list = [1,2,3]
+
+my_list2 = ['rock' , 'RNB' , 'old_school']
+
+my_list3 = ['Caroh' , 'Kambui' , '7876']
+
+my_tuple = ('Caroh' , 'Wanjiru' , 'Nyambura')
+
+my_set = set('Caroh' , 'Kambui' , '7876')
+
+my_dict = {'name':'Caroh' , 'age':19 , 'nationality':'Kenyan'}
+
+for a,b,c,d,e,for x in xrange(1,10):
+	pass in zip('my_list','my_list2','my_list3','my_tuple','my_set','my_dict'):
+    print a,b,c,d,e.for x in xrange(1,10):
+    	pass #1,2,3
+    	     #'rock' , 'RNB' , 'old_school'
+    	     #'Caroh' , 'Kambui' , '7876'
+    	     #'Caroh' , 'Wanjiru' , 'Nyambura'
+    	     #'Caroh' , 'Kambui' , '7876'
+    	     #'name':'Caroh' , 'age':19 , 'nationality':'Kenyan'
